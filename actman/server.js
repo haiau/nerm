c2090c52895b73c88b934b0df208660b5e26ee70
haiau <pnhaiau@gmail.com>
2017-03-04 15:26:47 +0700

    Merge remote-tracking branch 'origin/examp' (reverted from commit a16a980abebf8d520374af3790d7f82470c18a24)

---
+++ actman/server.js
@@ -0,0 +1,13 @@
+require('babel-register')({
+  presets: ['react']
+});
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use(require('./routes/index.jsx'));
+
+var PORT = 3000;
+app.listen(PORT, function() {
+  console.log('http://localhost:' + PORT);
+});
